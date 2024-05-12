34e136473f9190a72bb4833e92042c8b0636fb8c
Antoine 3000 <antoine@beaurin.net>
2024-05-12 16:28:33 +0200

    wip: pokemon list

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,28 @@
-// since there's no dynamic data here, we can prerender
-// it so that it gets served as a static asset in production
-export const prerender = true;
+import type { PageLoad } from './$types';
+
+interface PokemonMetadata {
+	name: string;
+	image: string;
+}
+
+export const load: PageLoad = async ({ fetch }) => {
+	const pokemonsListReq = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+	const pokemonsList = await pokemonsListReq.json();
+
+	let pokemons: PokemonMetadata[] = [];
+
+	for (let i = 0; i < pokemonsList.results.length; i++) {
+		const name = pokemonsList.results[i].name;
+		const pokemonInfoReq = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+		const pokemonInfo = pokemonInfoReq.json();
+		const image = pokemonInfo.sprites.front_default;
+
+		const pokemon: PokemonMetadata = {
+			name: name,
+			image: image
+		};
+
+		pokemons = [...pokemons, pokemon];
+	}
+	return { pokemons };
+};
