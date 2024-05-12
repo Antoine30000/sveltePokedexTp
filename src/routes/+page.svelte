7742ec962b7e04cef5924327df014c5c9b0886b8
Antoine 3000 <antoine@beaurin.net>
2024-05-12 16:38:31 +0200

    feat: workign images for pokemons list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,12 @@
     const pokemons = $page.data.pokemons;
 </script>
 
-<div class="flex-conrtainer">
+<div class="flex-container">
 	{#each pokemons as pokemon}
 	<!-- <a href="./{pokemon.name}"> -->
-		<div class="flex">
-		  <img src="{pokemon.sprite}" alt="image of {pokemon.name}">
+		<div class="flex-container">
+		  <!-- svelte-ignore a11y-missing-attribute -->
+		  <img src="{pokemon.image}">
 		  <span>{pokemon.name}</span>
 		</div>
 	  <!-- </a> -->
